e172d3822ee04e81db399c3f33b169eb03d24a30
HaoPhong11 <nguyenhaophong227@gmail.com>
2026-03-19 03:54:09 +0700

    update database

--- src/main/java/com/example/bookstore_db/dto/ReviewRequest.java
+++ src/main/java/com/example/bookstore_db/dto/ReviewRequest.java
@@ -7,27 +7,5 @@ public class ReviewRequest {
     private int rating; // từ 1 đến 5
     private String comment;
 
-    public String getBookId() {
-        return bookId;
-    }
 
-    public void setBookId(String bookId) {
-        bookId = bookId;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
 }
